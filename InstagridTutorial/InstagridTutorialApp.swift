8492c4f570d019bf7eb2f15338486c5e3d607bbe
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-02-16 16:11:19 +0100

    Initial Commit

---
+++ InstagridTutorial/InstagridTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagridTutorialApp.swift
+//  InstagridTutorial
+//
+//  Created by Djiveradjane Canessane on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagridTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
